87dfd5fd138984fc3f1dd73aaf872ba3619d15ec
Y-Jacob Mlo <y-jacobmlo@AdminisatorsMBP.lan>
2022-08-30 08:58:13 -0400

    first commit

---
+++ gatewayv2/script.js
@@ -0,0 +1,10 @@
+/* OPEN NAV */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+
+}
+/* Close Nav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
